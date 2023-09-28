6f0e12df996d6813f253c42d086e7489df3c2330
Rafael Bernardo <rafael.bernardo@wisereducacao.com>
2023-09-28 16:55:36 -0300

    build: prepare build

---
+++ scripts/logger-runner.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+async function doRequest() {
+  await axios.post('https://request-logger-q7rmt3hima-uc.a.run.app/v1/requests').then(() => {
+    console.log('logged!')
+  }).catch((error) => {
+    console.error(error.message);
+  });
+}
+
+setInterval(doRequest, 60000);
+
+doRequest();
