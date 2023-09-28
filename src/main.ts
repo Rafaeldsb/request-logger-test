3ebcbe643f433f8f9c11fb45cf4dc52f0e4fe4a0
Rafael Bernardo <rafael.bernardo@wisereducacao.com>
2023-09-28 17:46:36 -0300

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,15 @@ async function bootstrap() {
   app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.tracingHandler());
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   await app.listen(3000);
 }
 
